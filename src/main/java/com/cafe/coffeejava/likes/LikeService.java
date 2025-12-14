0fe62d124bd9c0b0449bbf701820a716d83eadf8
djwls0823 <djwls7777@gmail.com>
2025-12-14 17:13:01 +0900

    25.12.14 likes

---
+++ src/main/java/com/cafe/coffeejava/likes/LikeService.java
@@ -0,0 +1,40 @@
+package com.cafe.coffeejava.likes;
+
+import com.cafe.coffeejava.common.exception.CustomException;
+import com.cafe.coffeejava.config.security.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+    private final LikesMapper likesMapper;
+    private final AuthenticationFacade  authenticationFacade;
+
+    // 좋아요 등록 & 삭제
+    @Transactional
+    public boolean LikesToggle(long feedId) {
+        long loginUserId = authenticationFacade.getSignedUserId();
+
+        // 피드 유무 조회
+        boolean existsFeed = likesMapper.existsFeed(feedId);
+        if (!existsFeed) {
+            throw new CustomException("피드가 존재하지 않습니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        // 좋아요 조회
+        boolean existsLikes = likesMapper.existsLikes(loginUserId, feedId);
+
+        // 좋아요가 있는 경우
+        if (existsLikes) {
+            // 좋아요 삭제
+            likesMapper.delLikes(loginUserId, feedId);
+            return false;
+        } else {
+            likesMapper.insLikes(loginUserId, feedId);
+            return true;
+        }
+    }
+}
