4832825ac34ccd733db4f28b4f3fa885735e995e
djwls0823 <djwls7777@gmail.com>
2025-12-16 15:46:04 +0900

    25.12.16 신고 유형 불러오기

--- src/main/java/com/cafe/coffeejava/report/ReportController.java
+++ src/main/java/com/cafe/coffeejava/report/ReportController.java
@@ -2,14 +2,14 @@ package com.cafe.coffeejava.report;
 
 import com.cafe.coffeejava.common.model.ResultResponse;
 import com.cafe.coffeejava.report.model.ReportPostReq;
+import com.cafe.coffeejava.report.model.ReportTypeGetRes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/report")
@@ -27,6 +27,18 @@ public class ReportController {
                              .statusCode(String.valueOf(HttpServletResponse.SC_CREATED))
                              .resultMsg("신고 등록 성공")
                              .resultData(result)
+                             .build();
+    }
+
+    @GetMapping("/reportType")
+    @Operation(summary = "신고 유형 불러오기")
+    public ResultResponse<List<ReportTypeGetRes>> getReportType() {
+        List<ReportTypeGetRes> list = reportService.getReportType();
+
+        return ResultResponse.<List<ReportTypeGetRes>>builder()
+                             .statusCode(String.valueOf(HttpServletResponse.SC_OK))
+                             .resultMsg("신고 유형 조회 성공")
+                             .resultData(list)
                              .build();
     }
 }
---
+++ src/main/java/com/cafe/coffeejava/report/model/ReportTypeGetRes.java
@@ -0,0 +1,11 @@
+package com.cafe.coffeejava.report.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReportTypeGetRes {
+    private long reportTypeId;
+    private String reportTypeName;
+}
